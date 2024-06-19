380996733466212f2906f6e68f827c87bacac9d9
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-06-19 20:02:12 +0200

    Add files via upload
    Implementirana rekurzija i cell range atributi
    i dalje se testira

--- Parserdd.py
+++ Parserdd.py
@@ -4,6 +4,27 @@ from funkcije import *
 import re
 
 funkcije = ['SUM', 'AVERAGE', 'MAX', 'MIN', 'PRODUCT', 'IF', 'AND', 'OR', 'NOT']
+i = 0
+
+def transform_intervals(tokens : list) -> list:
+    new_list = []
+    j = 1
+    last = tokens[0]
+    for j in range(len(tokens)):
+        if tokens[j] == ':':
+            simplified = cell_range(str(tokens[j - 1]) + str(tokens[j]) + str(tokens[j + 1]))
+            simplified.pop(len(simplified) - 1)
+            #print(tokens)
+            #print(simplified)
+            new_list.extend(simplified)
+        else:
+            if j == 0 or j == len(tokens) - 1:
+                new_list.append(tokens[j])
+                continue
+            if tokens[j - 1] != ':' and tokens[j + 1] != ':':
+                new_list.append(tokens[j])
+
+    return new_list
 
 def tokenize(expression):
     tokens = []
@@ -43,6 +64,7 @@ def increment_char(char):
 
 def cell_range(range : str):
     left, right = split_string_by_colon(range)
+    print(left)
     x, y = split_letter_number(left)
     x1, y1 = split_letter_number(right)
     values = []
@@ -66,71 +88,79 @@ def is_excel_cell_format(s):
     pattern = re.compile(r'^[A-Z]+[0-9]+$')
     return bool(pattern.match(s))
 
-def get_argument(tokens : str, i : int) -> tuple[str, int]: #sta ako 3 + sum(1,2)
+def get_argument(tokens : list):
+    global i
     argument = ""
-    while i < len(tokens) and tokens[i] != ',':
-        if (is_excel_cell_format(tokens[i])):
-            a, b = split_letter_number(tokens[i])
-            argument += str(d.cells[a][b].value)
-        elif (tokens[i] in "*/+-" or is_number(tokens[i])):
+    while i < len(tokens):
+        if tokens[i] == ',':
+            break
+        if is_number(tokens[i]) or tokens[i] in "*/+-":
             argument += tokens[i]
+        elif is_excel_cell_format(tokens[i]):
+            alpha, digit = split_letter_number(tokens[i])
+            argument += str(d.cells[alpha][digit].value)
         elif tokens[i] in funkcije:
-            argument += str(calculate_expression(tokens, i))
+            last_i = i
+            tokens_copy = ""
+            j = i
+            while j < len(tokens):
+                if tokens[j] == ')':
+                    tokens_copy += ')'
+                    break
+                tokens_copy += tokens[j]
+                j+=1
+            tokens_copy = tokenize(tokens_copy)
+            #print(tokens_copy)
+            i = 0
+            argument += str(evaluate_expression(tokens_copy))
+            i = last_i
             while tokens[i] != ')':
                 i+=1
-            i += 2
             #print(i)
-            print(argument)
             continue
         i+=1
-    #print(argument)
-    return eval(argument), i
+    return eval(argument)
 
-
-def get_args(tokens : str, i : int):
+def get_arguments(tokens : list):
+    global i
     arguments = []
-    while i < len(tokens) and tokens[i] != ')':
-        argument, i = get_argument(tokens, i)
-        arguments.append(argument)
+    while i < len(tokens):
+        if tokens[i] == ')':
+            break
+        arguments.append(get_argument(tokens))
         i+=1
-    return arguments, i
-            
+    return arguments
 
-def calculate_expression(tokens : list, i : int) -> str:
+def evaluate_expression(tokens : list):
     sum = 0
-    if i >= len(tokens):
-        return
-    if is_number(tokens[i]):
-        if i == len(tokens) - 1:
-            return tokens[i]
-        else:
-            return eval(tokens[i] + "" + tokens[i] + calculate_expression(tokens, i + 2))
-    if tokens[i] in funkcije:
-        last = tokens[i]
-        args, i = get_args(tokens, i + 2)
-        if last == 'SUM':
-            sum += SUM(args)
-        elif last == 'AVERAGE':
-            sum += AVERAGE(args)
-        elif last == 'MAX':
-            sum += MAX(args)
-        elif last == 'MIN':
-            sum += MIN(args)
-        elif last == 'PRODUCT':
-            sum += PRODUCT(args)
+    global i
+    while i < len(tokens):
+        if tokens[i] in funkcije:
+            function_name = tokens[i]
+            i+=2
+            arguments = get_arguments(tokens)
+            print(arguments)
+            if function_name == 'SUM':
+                sum += SUM(arguments)
+            elif function_name == 'AVERAGE':
+                sum += AVERAGE(arguments)
+            elif function_name == 'MAX':
+                sum += MAX(arguments)
+            elif function_name == 'MIN':
+                sum += MIN(arguments)
+            elif function_name == 'PRODUCT':
+                sum += PRODUCT(arguments)
+        i+=1
     return sum
 
-
-expression = 'PRODUCT(SUM(A1, A2, A3), A2, A3)'
+expression = 'PRODUCT(A1:A3) + MAX(A1, A2)'
 tokens = tokenize(expression)
-
 print(tokens)
-
+tokens = transform_intervals(tokens)
+print(tokens)
 sum = 0
-
 d = Spreadsheet(100, 100)
 d.generate_cell('A', 1, 1)
-d.generate_cell('A', 2, 3)
+d.generate_cell('A', 2, 11)
 d.generate_cell('A', 3, 4)
-
-print(calculate_expression(tokens, 0))
+print(evaluate_expression(tokens))
