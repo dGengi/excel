5a36622aae0e12a3b491d045232e34f6013bec45
MilosVerbic <msvrbc@gmail.com>
2024-06-20 12:41:06 +0200

    Add files via upload
    parsiranje funkcija

--- Parserdd.py
+++ Parserdd.py
@@ -1,15 +1,16 @@
 from column_conversion import *
-from structure import *
+#from structure import *
 from funkcije import *
 import re
+from spreadsheet import SpreadsheetApp
 
 funkcije = ['SUM', 'AVERAGE', 'MAX', 'MIN', 'PRODUCT', 'IF', 'AND', 'OR', 'NOT']
 i = 0
 
 def transform_intervals(tokens : list) -> list:
     new_list = []
-    j = 1
-    last = tokens[0]
+    #j = 1 # ovo ne radi nista
+    #last = tokens[0] # ovo se ne koristi
     for j in range(len(tokens)):
         if tokens[j] == ':':
             simplified = cell_range(str(tokens[j - 1]) + str(tokens[j]) + str(tokens[j + 1]))
@@ -56,11 +57,11 @@ def split_letter_number(reference):
     else:
         return None, None
     
-def increment_char(char):
-    ascii_value = ord(char)
-    incremented_ascii_value = ascii_value + 1
-    incremented_char = chr(incremented_ascii_value)
-    return incremented_char
+#def increment_char(char): # nije potrebno (koristis csti i cits)
+#    ascii_value = ord(char)
+#    incremented_ascii_value = ascii_value + 1
+#    incremented_char = chr(incremented_ascii_value)
+#    return incremented_char
 
 def cell_range(range : str):
     left, right = split_string_by_colon(range)
@@ -68,13 +69,17 @@ def cell_range(range : str):
     x, y = split_letter_number(left)
     x1, y1 = split_letter_number(right)
     values = []
-    while x <= x1:
+
+    x, x1 = cits(min(csti(x),csti(x1))), cits(max(csti(x),csti(x1)))
+    y, y1 = min(y, y1), max(y, y1)
+
+    while csti(x) <= csti(x1):
         i = y
         while i <= y1:
             values.append(f"{x}{i}")
             values.append(",")
             i+=1
-        x = increment_char(x)
+        x = cits(csti(x)+1)
     return values
     
 def is_number(s):
@@ -88,79 +93,118 @@ def is_excel_cell_format(s):
     pattern = re.compile(r'^[A-Z]+[0-9]+$')
     return bool(pattern.match(s))
 
-def get_argument(tokens : list):
-    global i
-    argument = ""
-    while i < len(tokens):
-        if tokens[i] == ',':
-            break
-        if is_number(tokens[i]) or tokens[i] in "*/+-":
-            argument += tokens[i]
-        elif is_excel_cell_format(tokens[i]):
-            alpha, digit = split_letter_number(tokens[i])
-            argument += str(d.cells[alpha][digit].value)
-        elif tokens[i] in funkcije:
-            last_i = i
-            tokens_copy = ""
-            j = i
-            while j < len(tokens):
-                if tokens[j] == ')':
-                    tokens_copy += ')'
-                    break
-                tokens_copy += tokens[j]
-                j+=1
-            tokens_copy = tokenize(tokens_copy)
-            #print(tokens_copy)
-            i = 0
-            argument += str(evaluate_expression(tokens_copy))
-            i = last_i
-            while tokens[i] != ')':
-                i+=1
-            #print(i)
-            continue
-        i+=1
-    return eval(argument)
+#expression = 'MAX(1, 3) - PRODUCT(1000, 20)'
+#tokens = tokenize(expression)
+#tokens = transform_intervals(tokens)
+#print(tokens)
+#sum = 0
+#d = Spreadsheet()
+#d.generate_cell('A', 1, 1)
+#d.generate_cell('A', 2, 11)
+#d.generate_cell('A', 3, 4)
 
-def get_arguments(tokens : list):
-    global i
-    arguments = []
-    while i < len(tokens):
-        if tokens[i] == ')':
-            break
-        arguments.append(get_argument(tokens))
-        i+=1
-    return arguments
+def transform_cells(tokens, ss):
+    for i in range(len(tokens)):
+        if is_excel_cell_format(tokens[i]):
+            col, row = split_letter_number(tokens[i])
+            col = csti(col) - 1
+            row -= 1
+            tokens[i] = ss.variables[(row, col)].get()
+    return tokens
 
-def evaluate_expression(tokens : list):
-    sum = 0
-    global i
+def transform_eq(tokens):
+    for i in range(len(tokens)):
+        if tokens[i] == "=":
+            tokens[i] = "=="
+    return tokens
+
+def transform(tokens, ss = None):
+    tokens = transform_intervals(tokens)
+    tokens = transform_cells(tokens, ss)
+    tokens = transform_eq(tokens)
+    return tokens
+
+def izvrsi(tokens, ss: SpreadsheetApp | None = None):
+    tokens = transform(tokens, ss)
+    tokens2 = []
+    i = 0
     while i < len(tokens):
         if tokens[i] in funkcije:
-            function_name = tokens[i]
-            i+=2
-            arguments = get_arguments(tokens)
-            print(arguments)
-            if function_name == 'SUM':
-                sum += SUM(arguments)
-            elif function_name == 'AVERAGE':
-                sum += AVERAGE(arguments)
-            elif function_name == 'MAX':
-                sum += MAX(arguments)
-            elif function_name == 'MIN':
-                sum += MIN(arguments)
-            elif function_name == 'PRODUCT':
-                sum += PRODUCT(arguments)
-        i+=1
-    return sum
+            f = tokens[i]
+            z=1
+            for t in range(i+2, len(tokens)):
+                if tokens[t] == '(': z+=1
+                elif tokens[t] == ')':
+                    z-=1
+                    if z == 0:
+                        kraj = t
+                        break
+            l1 = tokens[(i+2):kraj]
+            #print("l1 =", l1)
+            l2 = []
+            j = 0
+            while j < len(l1):
+                if l1[j] in funkcije:
+                    z=1
+                    for t in range(j+2, len(l1)):
+                        if l1[t] == '(': z+=1
+                        elif l1[t] == ')':
+                            z-=1
+                            if z == 0:
+                                kraj2 = t
+                                break
+                    l2.append(l1[j:kraj2+1])
+                    j = kraj2
+                elif l1[j] != ',':
+                    z = 0
+                    zarez = j
+                    while zarez < len(l1):
+                        if l1[zarez] == '(': z += 1
+                        elif l1[zarez] == ')':
+                            z -= 1
+                            if z<0: print("ne valja nesto")
+                        elif l1[zarez] == ',' and z == 0:
+                            break
+                        zarez += 1
+                    l2.append(l1[j:zarez])
+                    j = zarez
+                j+=1
+            #print("l2 =", l2)
+            l3 = [izvrsi(token) for token in l2]
+            #print("l3 =", l3)
+            if f == "SUM":
+                tokens2.append(str(SUM(l3)))
+            elif f == "AVERAGE":
+                tokens2.append(str(AVERAGE(l3)))
+            elif f == "MAX":
+                tokens2.append(str(MAX(l3)))
+            elif f == "MIN":
+                tokens2.append(str(MIN(l3)))
+            elif f == "PRODUCT":
+                tokens2.append(str(PRODUCT(l3)))
+            elif f == "IF":
+                tokens2.append(str(IF(l3[0], l3[1], l3[2])))
+            elif f == "AND":
+                tokens2.append(str(AND(l3)))
+            elif f == "OR":
+                tokens2.append(str(OR(l3)))
+            elif f == "NOT":
+                tokens2.append(str(NOT(l3[0])))
+            i = kraj
+        else:
+            tokens2.append(tokens[i])
+        i += 1
+    new_expression = "".join(tokens2)
+    return str(eval(new_expression))
 
-expression = 'PRODUCT(A1:A3) + MAX(A1, A2)'
-tokens = tokenize(expression)
-print(tokens)
-tokens = transform_intervals(tokens)
-print(tokens)
-sum = 0
-d = Spreadsheet(100, 100)
-d.generate_cell('A', 1, 1)
-d.generate_cell('A', 2, 11)
-d.generate_cell('A', 3, 4)
-print(evaluate_expression(tokens))
+#e0 = "3+SUM(1,2)"
+#e1 = "SUM(1,3*AVERAGE(3,2,1))"
+#e2 = "PRODUCT(1, 2, SUM(3, 3, 5), 3)"
+#e3 = "IF(2=3, 5, -1)"
+#e4 = "NOT(AND(2=2, 3+3=6, OR(5+3=8, 2+2=5)))"
+
+#print(izvrsi(tokenize(e0)))
+#print(izvrsi(tokenize(e1)))
+#print(izvrsi(tokenize(e2)))
+#print(izvrsi(tokenize(e3)))
+#print(izvrsi(tokenize(e4)))
--- funkcije.py
+++ funkcije.py
@@ -5,30 +5,37 @@ from structure import Cell
 INT_MIN = -sys.maxsize - 1
 INT_MAX = sys.maxsize
 
-def SUM(cells: List[int]):
+def SUM(cells: List[str]):
+    #print(cells) #
+    cells2 = [float(cell) for cell in cells]
     sum = 0
-    for cell in cells:
+    for cell in cells2:
         sum += cell
     return sum
 
 def AVERAGE(cells):
-    return SUM(cells) / len(cells)
+    cells2 = [float(cell) for cell in cells]
+    return SUM(cells2) / len(cells2)
 
 def MAX(cells):
+    cells2 = [float(cell) for cell in cells]
     mx = INT_MIN
-    for cell in cells:
+    for cell in cells2:
         mx = max(mx, cell)
     return mx
 
 def MIN(cells):
+
+    cells2 = [float(cell) for cell in cells]
     mn = INT_MAX
-    for cell in cells:
+    for cell in cells2:
         mn = min(mn, cell)
     return mn
 
 def PRODUCT(cells):
+    cells2 = [float(cell) for cell in cells]
     product = 1
-    for cell in cells:
+    for cell in cells2:
         product *= cell
     return product
 
@@ -38,11 +45,15 @@ def IF(condition, true_result, false_result):
     else:
         return false_result
 
-def AND(*args):
-    return all(eval(arg) for arg in args if isinstance(arg, str))
+def AND(cells):
+    if 'False' in cells:
+        return False
+    return True
 
-def OR(*args):
-    return any(eval(arg) for arg in args if isinstance(arg, str))
+def OR(cells):
+    if 'True' in cells:
+        return True
+    return False
 
 def NOT(arg):
     return not eval(arg)
